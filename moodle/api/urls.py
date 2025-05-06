abbc2f3649d12b53e4498e318a8928f64e08a8dd
Yakoub <yakoubbenaissa@hotmail.com>
2025-05-06 21:50:16 +0100

    add resource retrieval endpoint to API URLs

--- moodle/api/urls.py
+++ moodle/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import fetch_courses, login, fetch_chapters, fetch_category
+from .views import fetch_courses, login, fetch_chapters, fetch_category, fetch_resource
 
 urlpatterns = [
     path("login/", login),
     path("categories/", fetch_category),
     path("courses/", fetch_courses),
     path("chapters/", fetch_chapters),
+    path("resource/", fetch_resource)
 ]
